ce9d633f94dec5b89d9d4e77749c8e5608c1e378
unknown <thaitrongtindev@gmail.com>
2023-11-11 17:31:27 +0700

    build adapter viewpager2

--- app/src/main/java/com/example/socialmediasmall/MainActivity.java
+++ app/src/main/java/com/example/socialmediasmall/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.socialmediasmall;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,17 +10,30 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.socialmediasmall.fragment.CreateAccountFragment;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private TabLa
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+
+    }
+    private void init() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager2 = findViewById(R.id.viewPager2);
+        tabLayout = findViewById(R.id.tabLayout);
     }
 }
---
+++ app/src/main/java/com/example/socialmediasmall/adapter/ViewPagerAdapter.java
@@ -0,0 +1,49 @@
+package com.example.socialmediasmall.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.socialmediasmall.fragment.AddFragment;
+import com.example.socialmediasmall.fragment.HomeFragment;
+import com.example.socialmediasmall.fragment.NotificationFragment;
+import com.example.socialmediasmall.fragment.ProfileFragment;
+import com.example.socialmediasmall.fragment.SearchFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    private int noOfTabs;
+
+    public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle, int noOfTabs) {
+        super(fragmentManager, lifecycle);
+        this.noOfTabs = noOfTabs;
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new SearchFragment();
+            case 2:
+                return new AddFragment();
+            case 3:
+                return new NotificationFragment();
+            case 4:
+                return new ProfileFragment();
+
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return noOfTabs;
+    }
+}
