c90432fb43694806984b0464b765884bb5009416
unknown <thaitrongtindev@gmail.com>
2023-11-11 18:24:47 +0700

    listener event click tablayout and viewpager2

--- app/src/main/java/com/example/socialmediasmall/MainActivity.java
+++ app/src/main/java/com/example/socialmediasmall/MainActivity.java
@@ -8,7 +8,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
 
+import com.example.socialmediasmall.adapter.ViewPagerAdapter;
 import com.example.socialmediasmall.fragment.CreateAccountFragment;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager2 viewPager2;
     private Toolbar toolbar;
+    private ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,57 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
+        // Tạo và đặt adapter cho ViewPager2
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), getLifecycle(), tabLayout.getTabCount());
+        viewPager2.setAdapter(viewPagerAdapter);
+
+        // Thêm listener cho sự kiện thay đổi trang trên ViewPager2
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                // Thay đổi tab được chọn khi trang ViewPager2 thay đổi
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
+        // Thêm tabs cho TabLayout
+        addTabs();
+
+        // Bắt sự kiện khi người dùng chọn một tab cụ thể
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // Di chuyển đến trang tương ứng khi người dùng chọn tab
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
     }
+
+
+
+    private void addTabs() {
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_home));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_search));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_add));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_heart));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_heart_fill));
+
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+
+    }
+
     private void init() {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
