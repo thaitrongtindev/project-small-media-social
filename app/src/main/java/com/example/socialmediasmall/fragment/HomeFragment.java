3ad8b3794142533f4b22b5d34f452dcb89f0c738
unknown <thaitrongtindev@gmail.com>
2023-11-13 17:45:40 +0700

    code home fragment

--- app/src/main/java/com/example/socialmediasmall/fragment/HomeFragment.java
+++ app/src/main/java/com/example/socialmediasmall/fragment/HomeFragment.java
@@ -15,6 +15,10 @@ import android.view.ViewGroup;
 import com.example.socialmediasmall.R;
 import com.example.socialmediasmall.adapter.HomeFragmentAdapter;
 import com.example.socialmediasmall.model.HomeModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +28,11 @@ public class HomeFragment extends Fragment {
     private RecyclerView recyclerView;
     private HomeFragmentAdapter homeFragmentAdapter;
     private List<HomeModel> mListHomeModels;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private FirebaseFirestore firebaseFirestore;
+    private DocumentReference reference;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,14 +52,33 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         init(view);
+
+        //document reference
+        reference = FirebaseFirestore.getInstance().collection("Posts").document(mUser.getUid());
+
         mListHomeModels = new ArrayList<>();
         homeFragmentAdapter = new HomeFragmentAdapter(mListHomeModels, getContext());
         recyclerView.setAdapter(homeFragmentAdapter);
+        
+        loadDatatFromFirebase();
+    }
+
+    private void loadDatatFromFirebase() {
+        mListHomeModels.add(new HomeModel("Trong Tin", "13/11/2023", "", "", "123456", 12));
+        mListHomeModels.add(new HomeModel("Van Chuong", "11/11/2023", "", "", "321654", 21));
+        mListHomeModels.add(new HomeModel("Trong Nghia", "13/11/2023", "", "", "478945", 10));
+        mListHomeModels.add(new HomeModel("Vuong Bui", "13/11/2023", "", "", "316542", 19));
+        mListHomeModels.add(new HomeModel("Thai Bao", "13/11/2023", "", "", "846453", 33));
+
+        homeFragmentAdapter.notifyDataSetChanged();
     }
 
     private void init(View view) {
         recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
     }
 }
